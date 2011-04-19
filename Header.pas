f364042c0d28bad7cb733f8db0cae07ef04f3783
perfectdaemon <perfectdaemon@a50c57a5-280c-c33b-a64e-00ac8074d581>
2011-04-19 16:52:52 +0000

    Pascal header changed - deprecated functions removed. SharpLoader project added. Now glrenderer is cross-PL-able :) Simple unmanaged code invoke used (DllImport directive).

--- Header.pas
+++ Header.pas
@@ -1,5 +1,9 @@
 {
   TODO: +1)функции для работы с данными.
+
+  2011-04-19 - Апдейт по текущему экспорту - glrenderer больше не экспортирует
+               функции для работы с данными, анимацией, а также с движением
+               камеры
 }
 unit Header;
 
@@ -28,55 +32,56 @@ var
   renderCameraSet: function(X, Y, Z, LookX, LookY, LookZ, UpX, UpY, UpZ: Single): Integer; stdcall;
   //Установка точки наблюдения с сохранением остальных параметров
   renderCameraSetTarget: function(LookX, LookY, LookZ: Single): Integer; stdcall;
-  //Плавное передвижение камеры к новой точке с максимальной скоростью MaxSpeed и ускорением Accel
-  renderCameraSetTargetMove: function(LookX, LookY, LookZ, MaxSpeed, Accel: Single): Integer; stdcall;
+//  //Плавное передвижение камеры к новой точке с максимальной скоростью MaxSpeed и ускорением Accel
+//  renderCameraSetTargetMove: function(LookX, LookY, LookZ, MaxSpeed, Accel: Single): Integer; stdcall;
   //Установка позиции камеры с сохранием остальных параметров
   renderCameraSetPos: function(X,Y,Z: Single): Integer; stdcall;
-  //Плавное перемещение позиции камеры с максимальной скоростью MaxSpeed и ускорением Accel с сохранением остальных параметров
-  renderCameraSetPosMove: function(X, Y, Z, MaxSpeed, Accel: Single): Integer; stdcall;
+//  //Плавное перемещение позиции камеры с максимальной скоростью MaxSpeed и ускорением Accel с сохранением остальных параметров
+//  renderCameraSetPosMove: function(X, Y, Z, MaxSpeed, Accel: Single): Integer; stdcall;
   //Установка вектора "верха" камеры с сохранением  остальных параметров
   renderCameraSetUp: function (UpX, UpY, UpZ: Single): Integer; stdcall;
-  //Плавное изменение вектора "верха" камеры с максимальной скоростью MaxSpeed и ускорением Accel с сохранением остальных параметров
-  renderCameraSetUpMove: function (UpX, UpY, UpZ, MaxSpeed, Accel: Single): Integer; stdcall;
-  //Вращение камеры вокруг позиции наблюдения с сохранением расстояния до нее
-  renderCameraMoveAroundTarget: function(HorDelta, VerDelta: Single): Integer; stdcall;
+//  //Плавное изменение вектора "верха" камеры с максимальной скоростью MaxSpeed и ускорением Accel с сохранением остальных параметров
+//  renderCameraSetUpMove: function (UpX, UpY, UpZ, MaxSpeed, Accel: Single): Integer; stdcall;
+//  //Вращение камеры вокруг позиции наблюдения с сохранением расстояния до нее
+//  renderCameraMoveAroundTarget: function(HorDelta, VerDelta: Single): Integer; stdcall;
 
-  //РАБОТА С ДАННЫМИ
-
-  //Загрузка данных для рендера из внешнего файла.
-  renderDataAddFromFile: function(FileName: PAnsiChar): Integer; stdcall;
-
-  renderDataSaveToFile: function(FileName: PAnsiChar): Integer; stdcall;
-
-  //Добавление сферы в позицию X, Y, Z радиусом Radius и цветом RGB.
-  //Возвращаемое значение - имя данной сферы для манипулирования
-  renderDataSphereAdd: function(X, Y, Z, Radius, R, G, B, A: Single): Integer; stdcall;
-  //Изменение позиции сферы с именем/номером Name
-  renderDataSpherePos: function(Name: Integer; X, Y, Z: Single): Integer; stdcall;
-  //Изменение позиции сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
-  renderDataSpherePosMove: function(Name: Integer; X, Y, Z, MaxSpeed, Accel: Single): Integer; stdcall;
-  //Изменение радиуса сферы с именем/номером Name
-  renderDataSphereRad: function(Name: Integer; Radius: Single): Integer; stdcall;
-  //Изменение радиуса сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
-  renderDataSphereRadMove: function(Name: Integer; Radius, MaxSpeed, Accel: Single): Integer; stdcall;
-  //Изменение цвета сферы с именем/номером Name
-  renderDataSphereRGB: function(Name: Integer; R, G, B, A: Single): Integer; stdcall;
-  //Изменение цвета сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
-  renderDataSphereRGBMove: function(Name: Integer; R, G, B, A,  MaxSpeed, Accel: Single): Integer; stdcall;
-
-  renderDataCylinderAdd: function(X, Y, Z, Radius, ALength, R, G, B, A, DirX, DirY, DirZ, UpX, UpY, UpZ: Single): Integer; stdcall;
-
-  //РАБОТА С АНИМАЦИЕЙ
-  renderAnimAddFromFile: function(FileName: PAnsiChar): Integer; stdcall;
-  renderAnimSetSpeed: function(Speed: Single): Integer; stdcall;
-  renderAnimGetSpeed: function: Single; stdcall;
-  renderAnimPlay: function: Integer; stdcall;
-  renderAnimPause: function: Integer; stdcall;
-  renderAnimStop: function: Integer; stdcall;
-  renderAnimNextBlock: function: Integer; stdcall;
-  renderAnimPrevBlock: function: Integer; stdcall;
-  renderAnimJumpToA: function(blockName: PAnsiChar): Integer; stdcall;
-  renderAnimJumpToB: function(blockNumber: Integer): Integer; stdcall;
+// Из текущей версии убрана реализация данных и анимации
+//  //РАБОТА С ДАННЫМИ
+//
+//  //Загрузка данных для рендера из внешнего файла.
+//  renderDataAddFromFile: function(FileName: PAnsiChar): Integer; stdcall;
+//
+//  renderDataSaveToFile: function(FileName: PAnsiChar): Integer; stdcall;
+//
+//  //Добавление сферы в позицию X, Y, Z радиусом Radius и цветом RGB.
+//  //Возвращаемое значение - имя данной сферы для манипулирования
+//  renderDataSphereAdd: function(X, Y, Z, Radius, R, G, B, A: Single): Integer; stdcall;
+//  //Изменение позиции сферы с именем/номером Name
+//  renderDataSpherePos: function(Name: Integer; X, Y, Z: Single): Integer; stdcall;
+//  //Изменение позиции сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
+//  renderDataSpherePosMove: function(Name: Integer; X, Y, Z, MaxSpeed, Accel: Single): Integer; stdcall;
+//  //Изменение радиуса сферы с именем/номером Name
+//  renderDataSphereRad: function(Name: Integer; Radius: Single): Integer; stdcall;
+//  //Изменение радиуса сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
+//  renderDataSphereRadMove: function(Name: Integer; Radius, MaxSpeed, Accel: Single): Integer; stdcall;
+//  //Изменение цвета сферы с именем/номером Name
+//  renderDataSphereRGB: function(Name: Integer; R, G, B, A: Single): Integer; stdcall;
+//  //Изменение цвета сферы с именем/номером Name с максимальной скоростью MaxSpeed и ускорением Accel
+//  renderDataSphereRGBMove: function(Name: Integer; R, G, B, A,  MaxSpeed, Accel: Single): Integer; stdcall;
+//
+//  renderDataCylinderAdd: function(X, Y, Z, Radius, ALength, R, G, B, A, DirX, DirY, DirZ, UpX, UpY, UpZ: Single): Integer; stdcall;
+//
+//  //РАБОТА С АНИМАЦИЕЙ
+//  renderAnimAddFromFile: function(FileName: PAnsiChar): Integer; stdcall;
+//  renderAnimSetSpeed: function(Speed: Single): Integer; stdcall;
+//  renderAnimGetSpeed: function: Single; stdcall;
+//  renderAnimPlay: function: Integer; stdcall;
+//  renderAnimPause: function: Integer; stdcall;
+//  renderAnimStop: function: Integer; stdcall;
+//  renderAnimNextBlock: function: Integer; stdcall;
+//  renderAnimPrevBlock: function: Integer; stdcall;
+//  renderAnimJumpToA: function(blockName: PAnsiChar): Integer; stdcall;
+//  renderAnimJumpToB: function(blockNumber: Integer): Integer; stdcall;
 
   //РАБОТА С ИСТОЧНИКОМ СВЕТА
 
@@ -116,35 +121,35 @@ initialization
 
   renderCameraSet := GetProcAddress(dllHandle, 'renderCameraSet');
   renderCameraSetTarget := GetProcAddress(dllHandle, 'renderCameraSetTarget');
-  renderCameraSetTargetMove := GetProcAddress(dllHandle, 'renderCameraSetTargetMove');
+//  renderCameraSetTargetMove := GetProcAddress(dllHandle, 'renderCameraSetTargetMove');
   renderCameraSetPos := GetProcAddress(dllHandle, 'renderCameraSetPos');
-  renderCameraSetPosMove := GetProcAddress(dllHandle, 'renderCameraSetPosMove');
+//  renderCameraSetPosMove := GetProcAddress(dllHandle, 'renderCameraSetPosMove');
   renderCameraSetUp := GetProcAddress(dllHandle, 'renderCameraSetUp');
-  renderCameraSetUpMove := GetProcAddress(dllHandle, 'renderCameraSetUpMove');
-  renderCameraMoveAroundTarget := GetProcAddress(dllHandle, 'renderCameraMoveAroundTarget');
+//  renderCameraSetUpMove := GetProcAddress(dllHandle, 'renderCameraSetUpMove');
+//  renderCameraMoveAroundTarget := GetProcAddress(dllHandle, 'renderCameraMoveAroundTarget');
 
-  renderDataAddFromFile := GetProcAddress(dllHandle, 'renderDataAddFromFile');
-  renderDataSaveToFile := GetProcAddress(dllhandle, 'renderDataSaveToFile');
-  renderDataSphereAdd := GetProcAddress(dllHandle, 'renderDataSphereAdd');
-  renderDataSpherePos := GetProcAddress(dllHandle, 'renderDataSpherePos');
-  renderDataSpherePosMove := GetProcAddress(dllHandle, 'renderDataSpherePosMove');
-  renderDataSphereRad := GetProcAddress(dllHandle, 'renderDataSphereRad');
-  renderDataSphereRadMove := GetProcAddress(dllHandle, 'renderDataSphereRadMove');
-  renderDataSphereRGB := GetProcAddress(dllHandle, 'renderDataSphereRGB');
-  renderDataSphereRGBMove := GetProcAddress(dllHandle, 'renderDataSphereRGBMove');
-
-  renderDataCylinderAdd := GetProcAddress(dllHandle, 'renderDataCylinderAdd');
-
-  renderAnimAddFromFile := GetProcAddress(dllHandle, 'renderAnimAddFromFile');
-  renderAnimSetSpeed := GetProcAddress(dllHandle, 'renderAnimSetSpeed');
-  renderAnimGetSpeed := GetProcAddress(dllHandle, 'renderAnimGetSpeed');
-  renderAnimPlay := GetProcAddress(dllHandle, 'renderAnimPlay');
-  renderAnimPause := GetProcAddress(dllHandle, 'renderAnimPause');
-  renderAnimStop := GetProcAddress(dllHandle, 'renderAnimStop');
-  renderAnimNextBlock := GetProcAddress(dllHandle, 'renderAnimNextBlock');
-  renderAnimPrevBlock := GetProcAddress(dllHandle, 'renderAnimPrevBlock');
-  renderAnimJumpToA := GetProcAddress(dllHandle, 'renderAnimJumpToA');
-  renderAnimJumpToB := GetProcAddress(dllHandle, 'renderAnimJumpToB');
+//  renderDataAddFromFile := GetProcAddress(dllHandle, 'renderDataAddFromFile');
+//  renderDataSaveToFile := GetProcAddress(dllhandle, 'renderDataSaveToFile');
+//  renderDataSphereAdd := GetProcAddress(dllHandle, 'renderDataSphereAdd');
+//  renderDataSpherePos := GetProcAddress(dllHandle, 'renderDataSpherePos');
+//  renderDataSpherePosMove := GetProcAddress(dllHandle, 'renderDataSpherePosMove');
+//  renderDataSphereRad := GetProcAddress(dllHandle, 'renderDataSphereRad');
+//  renderDataSphereRadMove := GetProcAddress(dllHandle, 'renderDataSphereRadMove');
+//  renderDataSphereRGB := GetProcAddress(dllHandle, 'renderDataSphereRGB');
+//  renderDataSphereRGBMove := GetProcAddress(dllHandle, 'renderDataSphereRGBMove');
+//
+//  renderDataCylinderAdd := GetProcAddress(dllHandle, 'renderDataCylinderAdd');
+//
+//  renderAnimAddFromFile := GetProcAddress(dllHandle, 'renderAnimAddFromFile');
+//  renderAnimSetSpeed := GetProcAddress(dllHandle, 'renderAnimSetSpeed');
+//  renderAnimGetSpeed := GetProcAddress(dllHandle, 'renderAnimGetSpeed');
+//  renderAnimPlay := GetProcAddress(dllHandle, 'renderAnimPlay');
+//  renderAnimPause := GetProcAddress(dllHandle, 'renderAnimPause');
+//  renderAnimStop := GetProcAddress(dllHandle, 'renderAnimStop');
+//  renderAnimNextBlock := GetProcAddress(dllHandle, 'renderAnimNextBlock');
+//  renderAnimPrevBlock := GetProcAddress(dllHandle, 'renderAnimPrevBlock');
+//  renderAnimJumpToA := GetProcAddress(dllHandle, 'renderAnimJumpToA');
+//  renderAnimJumpToB := GetProcAddress(dllHandle, 'renderAnimJumpToB');
 
   renderLightSet := GetProcAddress(dllHandle, 'renderLightSet');
   renderLightSetPos := GetProcAddress(dllHandle, 'renderLightSetPos');
